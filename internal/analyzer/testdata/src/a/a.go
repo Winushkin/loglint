220fa3ffe6c372adf0bb49e0bc384c01ef3bb730
Kirill Belyaev <kirill10121415@gmail.com>
2026-02-21 01:16:27 +0300

    feat(test): add tets data

--- internal/analyzer/testdata/src/a/a.go
+++ internal/analyzer/testdata/src/a/a.go
@@ -6,7 +6,7 @@ import (
 
 func test() {
 	slog.Debug("Starting server") // want "log message must start with lowercase letter"
-	slog.Info("Запуск")           // want "log message must be in English only"
+	slog.Info("запуск")           // want "log message must be in English only"
 	slog.Warn("!dadasdad!")       // want "log message contains special characters"
 	slog.Error("password")        // want "log message may contain sensitive data"
 }
